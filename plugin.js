4baddb25b1ef7cb0d0b88ad3ea1380c3cfbed248
James Sumners <james.sumners@gmail.com>
2017-09-10 16:47:27 -0400

    Use decorateReply (fixes #1)

--- plugin.js
+++ plugin.js
@@ -4,6 +4,12 @@ const fp = require('fastify-plugin')
 const cookie = require('cookie')
 
 function plugin (fastify, options, next) {
+  fastify.decorateReply('setCookie', function (name, value, options) {
+    const seriaized = cookie.serialize(name, value, options || {})
+    this.header('Set-Cookie', seriaized)
+    return this
+  })
+
   fastify.addHook('preHandler', (fastifyReq, fastifyRes, done) => {
     if (fastifyReq.cookies) done()
     if (!fastifyReq.req.headers) fastifyReq.req.headers = {}
@@ -11,12 +17,6 @@ function plugin (fastify, options, next) {
     const cookieHeader = fastifyReq.req.headers.cookie
     const cookies = (cookieHeader) ? cookie.parse(cookieHeader) : []
     fastifyReq.cookies = cookies
-
-    fastifyRes.setCookie = function (name, value, options) {
-      const seriaized = cookie.serialize(name, value, options || {})
-      this.header('Set-Cookie', seriaized)
-      return this
-    }
 
     done()
   })
