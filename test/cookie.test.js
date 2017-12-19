af6a0f6cfc6db7bce6c5ad07bd4cc31c4c3364d0
Denis Fäcke <serayaeryn@gmail.com>
2017-12-19 20:11:40 +0100

    fix handling of multiple cookies

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -43,6 +43,44 @@ test('cookies get set correctly', (t) => {
   })
 })
 
+test('should set multiple cookies', (t) => {
+  t.plan(8)
+  const fastify = Fastify()
+  fastify.register(plugin)
+
+  fastify.listen(0, (err) => {
+    if (err) tap.error(err)
+    fastify.server.unref()
+
+    const reqOpts = {
+      method: 'GET',
+      baseUrl: 'http://localhost:' + fastify.server.address().port
+    }
+    const req = request.defaults(reqOpts)
+
+    fastify.get('/', (req, reply) => {
+      reply
+        .setCookie('foo', 'foo')
+        .setCookie('bar', 'test')
+        .send({hello: 'world'})
+    })
+
+    const jar = request.jar()
+    req({uri: '/', jar}, (err, response, body) => {
+      t.error(err)
+      t.strictEqual(response.statusCode, 200)
+      t.deepEqual(JSON.parse(body), {hello: 'world'})
+
+      const cookies = jar.getCookies(reqOpts.baseUrl + '/')
+      t.is(cookies.length, 2)
+      t.is(cookies[0].key, 'foo')
+      t.is(cookies[0].value, 'foo')
+      t.is(cookies[1].key, 'bar')
+      t.is(cookies[1].value, 'test')
+    })
+  })
+})
+
 test('cookies get set correctly with millisecond dates', (t) => {
   t.plan(8)
   const fastify = Fastify()
