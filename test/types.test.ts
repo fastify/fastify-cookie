c16c87e8ff02a0750c1964d729ad4b84cedf381f
m03geek <s@zxc.pp.ua>
2018-11-06 09:05:57 +0200

    feat(types): add typescript definitions

---
+++ test/types.test.ts
@@ -0,0 +1,13 @@
+import fastify = require('fastify');
+import cookie = require('../');
+
+const app = fastify();
+
+app
+  .register(cookie)
+  .after(() => {
+    app.get('/', (request, reply) => {
+      const test = request.cookies.test;
+      reply.setCookie('test', test);
+    })
+  });
