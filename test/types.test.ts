649ab373f00b4443e7645c0f845f7fbd8a7fb77b
Allan Calix <contact@allancalix.com>
2018-12-12 21:56:36 -0800

    Add test for types with HTTP2

--- test/types.test.ts
+++ test/types.test.ts
@@ -1,12 +1,29 @@
 import fastify = require('fastify');
+import http2 = require('http2');
 import cookie = require('../');
 
-const app = fastify();
+const appWithImplicitHttp = fastify();
 
-app
+appWithImplicitHttp
   .register(cookie)
   .after(() => {
-    app.get('/', (request, reply) => {
+    appWithImplicitHttp.get('/', (request, reply) => {
+      const test = request.cookies.test;
+      reply.setCookie('test', test, { domain: 'example.com', path: '/' }).send({hello: 'world'});
+    })
+  });
+
+const appWithHttp2: fastify.FastifyInstance<
+  http2.Http2Server,
+  http2.Http2ServerRequest,
+  http2.Http2ServerResponse
+> = fastify();
+
+
+appWithHttp2
+  .register(cookie)
+  .after(() => {
+    appWithHttp2.get('/', (request, reply) => {
       const test = request.cookies.test;
       reply.setCookie('test', test, { domain: 'example.com', path: '/' }).send({hello: 'world'});
     })
