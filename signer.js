1bb39fb2e8241c442cfe87eb4a35f1dd66f69210
uzlopak <aras.abbasi@googlemail.com>
2022-07-18 10:49:36 +0200

    restructure SignerFactory

--- signer.js
+++ signer.js
@@ -2,90 +2,95 @@
 
 const crypto = require('crypto')
 
-function signerFactory (secret, factoryAlgorithm = 'sha256') {
-  const secrets = Array.isArray(secret) ? secret : [secret]
+function SignerFactory (secret, algorithm = 'sha256') {
+  if (!(this instanceof SignerFactory)) {
+    return new SignerFactory(secret, algorithm)
+  }
 
+  this.secrets = Array.isArray(secret) ? secret : [secret]
+  this.signingKey = this.secrets[0]
+  this.algorithm = algorithm
+
+  validateSecrets(this.secrets)
+  validateAlgorithm(this.algorithm)
+}
+
+function validateSecrets (secrets) {
   for (const secret of secrets) {
     if (typeof secret !== 'string') {
       throw new TypeError('Secret key must be a string.')
     }
   }
+}
 
-  const signingKey = secrets[0]
-
+function validateAlgorithm (algorithm) {
   // validate that the algorithm is supported by the node runtime
   try {
-    crypto.createHmac(factoryAlgorithm, signingKey)
+    crypto.createHmac(algorithm, 'dummyHmac')
   } catch (e) {
-    throw new TypeError(`Algorithm ${factoryAlgorithm} not supported.`)
+    throw new TypeError(`Algorithm ${algorithm} not supported.`)
+  }
+}
+
+SignerFactory.prototype.sign = function (value, secret = this.signingKey, algorithm = this.algorithm) {
+  if (typeof value !== 'string') {
+    throw new TypeError('Cookie value must be provided as a string.')
+  }
+  return value + '.' + crypto
+    .createHmac(algorithm, secret)
+    .update(value)
+    .digest('base64')
+    // remove base64 padding (=) as it has special meaning in cookies
+    .replace(/=+$/, '')
+}
+
+SignerFactory.prototype.unsign = function (signedValue, secrets = this.secrets, algorithm = this.algorithm) {
+  if (typeof signedValue !== 'string') {
+    throw new TypeError('Signed cookie string must be provided.')
+  }
+  const value = signedValue.slice(0, signedValue.lastIndexOf('.'))
+  const actual = Buffer.from(signedValue)
+
+  for (const secret of secrets) {
+    const expected = Buffer.from(this.sign(value, secret, algorithm))
+    if (
+      expected.length === actual.length &&
+      crypto.timingSafeEqual(expected, actual)
+    ) {
+      return {
+        valid: true,
+        renew: secret !== secrets[0],
+        value
+      }
+    }
   }
 
   return {
-    sign (value, secret = signingKey, algorithm = factoryAlgorithm) {
-      if (typeof value !== 'string') {
-        throw new TypeError('Cookie value must be provided as a string.')
-      }
-      return value + '.' + crypto
-        .createHmac(algorithm, secret)
-        .update(value)
-        .digest('base64')
-        // remove base64 padding (=) as it has special meaning in cookies
-        .replace(/=+$/, '')
-    },
-    unsign (signedValue, keys = secrets, algorithm = factoryAlgorithm) {
-      if (typeof signedValue !== 'string') {
-        throw new TypeError('Signed cookie string must be provided.')
-      }
-      const value = signedValue.slice(0, signedValue.lastIndexOf('.'))
-      const actual = Buffer.from(signedValue)
-
-      for (const key of keys) {
-        const expected = Buffer.from(this.sign(value, key, algorithm))
-        if (
-          expected.length === actual.length &&
-          crypto.timingSafeEqual(expected, actual)
-        ) {
-          return {
-            valid: true,
-            renew: key !== keys[0],
-            value
-          }
-        }
-      }
-
-      return {
-        valid: false,
-        renew: false,
-        value: null
-      }
-    }
+    valid: false,
+    renew: false,
+    value: null
   }
 }
 
 // create a signer-instance, with dummy secret
-const signer = signerFactory(['dummy'])
+const signer = new SignerFactory(['dummy'])
 
-module.exports = signerFactory
-module.exports.signerFactory = signerFactory
-module.exports.sign = function sign (value, secret, algorithm = 'sha256') {
+function sign (value, secret, algorithm = 'sha256') {
   const secrets = Array.isArray(secret) ? secret : [secret]
 
-  for (const secret of secrets) {
-    if (typeof secret !== 'string') {
-      throw new TypeError('Secret key must be a string.')
-    }
-  }
+  validateSecrets(secrets)
 
   return signer.sign(value, secrets[0], algorithm)
 }
-module.exports.unsign = function unsign (signedValue, secret, algorithm = 'sha256') {
+
+function unsign (signedValue, secret, algorithm = 'sha256') {
   const secrets = Array.isArray(secret) ? secret : [secret]
 
-  for (const secret of secrets) {
-    if (typeof secret !== 'string') {
-      throw new TypeError('Secret key must be a string.')
-    }
-  }
+  validateSecrets(secrets)
 
   return signer.unsign(signedValue, secrets, algorithm)
 }
+module.exports = SignerFactory
+module.exports.signerFactory = SignerFactory
+module.exports.sign = sign
+module.exports.unsign = unsign
