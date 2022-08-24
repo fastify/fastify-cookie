01a33e4a798279d5e3473b858e89a4d5fc080b50
vitaly-sazonov <62155164+vitaly-sazonov@users.noreply.github.com>
2022-08-24 20:01:06 +0300

    Update test/cookie.test.js
    Co-authored-by: Manuel Spigolon <behemoth89@gmail.com>

--- test/cookie.test.js
+++ test/cookie.test.js
@@ -889,7 +889,6 @@ test('result in an error if hook-option is set to an invalid value', (t) => {
   t.rejects(
     () => fastify.register(plugin, { hook: true }),
     {},
-    { skip: true },
     new Error("@fastify/cookie: Invalid value provided for the hook-option. You can set the hook-option only to false, 'onRequest' , 'preParsing' , 'preValidation' or 'preHandler'")
   )
 })
