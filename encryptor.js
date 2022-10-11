83b55a6f6a061a9834a0365b3efa9e669b6703d7
Gün <74139498+gunwd@users.noreply.github.com>
2022-10-11 15:14:36 +0200

    refactor

--- encryptor.js
+++ encryptor.js
@@ -4,23 +4,23 @@ const crypto = require('crypto')
 const defaultAlgorithm = 'aes256'
 const defaultEncoding = 'base64'
 
-function Encryptor (key) {
-  this.key = key || crypto.randomBytes(32)
+function Encryptor (key, algorithm, encoding) {
+  this.key = key
+  this.algorithm = algorithm || defaultAlgorithm
+  this.encoding = encoding || defaultEncoding
+
+  checkProps(this.key, this.algorithm)
 }
 
 Encryptor.prototype.encrypt = function (value) {
-  return encrypt(value, this.key)
+  return encrypt(value, this.key, this.algorithm, this.encoding)
 }
 
 Encryptor.prototype.decrypt = function (value) {
-  return decrypt(value, this.key)
+  return decrypt(value, this.key, this.algorithm, this.encoding)
 }
 
-function encrypt (str, key) {
-  const algorithm = defaultAlgorithm
-
-  checkProps(algorithm, key)
-
+function encrypt (str, key, algorithm, encoding) {
   const iv = crypto.randomBytes(16)
   const cipher = crypto.createCipheriv(
     algorithm,
@@ -28,25 +28,21 @@ function encrypt (str, key) {
     iv
   )
 
-  const encrypted = `${iv.toString(defaultEncoding)}:${cipher.update(str, 'utf-8', defaultEncoding) + cipher.final(defaultEncoding)}`
+  const encrypted = `${iv.toString(encoding)}:${cipher.update(str, 'utf-8', encoding) + cipher.final(encoding)}`
   return encrypted
 }
 
-function decrypt (str, key) {
-  const algorithm = defaultAlgorithm
-
-  checkProps(algorithm, key)
-
+function decrypt (str, key, algorithm, encoding) {
   const encryptedArray = str.split(':')
-  const iv = Buffer.from(encryptedArray[0], defaultEncoding)
-  const encrypted = Buffer.from(encryptedArray[1], defaultEncoding)
+  const iv = Buffer.from(encryptedArray[0], encoding)
+  const encrypted = Buffer.from(encryptedArray[1], encoding)
   const decipher = crypto.createDecipheriv(algorithm, key, iv)
 
-  const decrypted = decipher.update(encrypted, defaultEncoding, 'utf-8') + decipher.final('utf-8')
+  const decrypted = decipher.update(encrypted, encoding, 'utf-8') + decipher.final('utf-8')
   return decrypted
 }
 
-function checkProps (algorithm, key) {
+function checkProps (key, algorithm) {
   if (!key) {
     throw new TypeError('A key has to be supplied.')
   }
